2464972ad97a2188ae60b64c77a6ab370dcc82a6
Peter Grainger <peter@grainger.xyz>
2020-01-03 07:32:48 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,9 @@
 #!/bin/sh -l
 
-ncc build $1
+if test -f $1 ; then
+  ncc build $1
+  exit 0
+else
+  echo "$1 does not exist. Make sure you used the checkout action and that if your entry file is not src/index.js that the custom entry-file argument is set in the workflow yml file."
+  exit 1
+fi
